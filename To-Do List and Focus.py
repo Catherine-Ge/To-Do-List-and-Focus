09c6aa4de8a66622f6d8cdc69a8e37397a5e8a2c
Catherine-Ge <95162919+Catherine-Ge@users.noreply.github.com>
2022-04-26 11:03:26 -0400

    Update To-Do List and Focus.py
    Fixed a bug inside dlt().
    Previously, if I select nothing from the listbox, there would be an error. So I made the program to tell if a task is selected and if there is nothing selected, it pops up a warning.

--- To-Do List and Focus.py
+++ To-Do List and Focus.py
@@ -40,17 +40,20 @@ def cmplt():
     listbox.delete(ANCHOR)
 
 def dlt():
-    value=listbox.get(listbox.curselection())
-    listbox.delete(ANCHOR)
-    file = open("todo.txt", "r")
-    lines = file.readlines()
-    file.close()
-    file = open("todo.txt", "w")
-    for line in lines:
-        if line != (value+"\n"):
-            file.write(line)
-    file.close()
-
+    if listbox.curselection() == ():
+        messagebox.showwarning("app", "Please select a task")
+    else:
+        value=listbox.get(listbox.curselection())
+        listbox.delete(ANCHOR)
+        file = open("todo.txt", "r")
+        lines = file.readlines()
+        file.close()
+        file = open("todo.txt", "w")
+        for line in lines:
+            if line != (value+"\n"):
+                file.write(line)
+        file.close()
+        
 def load():
     listbox.delete(0, END)
     file = open("todo.txt", "r")
